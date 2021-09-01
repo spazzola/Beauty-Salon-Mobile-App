b246d67d57c24f05158022aa064e0f8e931bb05f
Rafal Szczotka <rafalszczotka@wp.pl>
2021-09-01 14:52:51 +0200

    Added CreateAppointmentRequest and CreateWorkRequest

---
+++ src/main/java/pomalowane/appointment/CreateAppointmentRequest.java
@@ -0,0 +1,24 @@
+package pomalowane.appointment;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAppointmentRequest {
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime startDate;
+    private Long clientId;
+    private Long employeeId;
+    private List<Long> workIds;
+
+}
---
+++ src/main/java/pomalowane/work/CreateWorkRequest.java
@@ -0,0 +1,19 @@
+package pomalowane.work;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateWorkRequest {
+
+    private String name;
+    private BigDecimal price;
+    private int hoursDuration;
+    private int minutesDuration;
+
+}
