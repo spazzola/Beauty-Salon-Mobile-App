f2d00dcd134a46280881af5f0f9d969479cedf60
Rafal Szczotka <rafalszczotka@wp.pl>
2022-02-16 00:17:09 +0000

    Refactor SmsService, changed configuration to production

--- src/main/java/pomalowane/sms/SmsService.java
+++ src/main/java/pomalowane/sms/SmsService.java
@@ -19,24 +19,35 @@ public class SmsService {
 
     private SmsDao smsDao;
 
+    private final String apiLogin = "webapi_merios";
+    private final String apiPassword = "4069ce21";
+
 
     public void setSmsReminder(Appointment appointment) throws Exception {
         List<Sms> smsReminders = new ArrayList<>();
-        String dayBefore = getDateDayBefore(appointment);
-        Sms smsDayBefore = buildSms(appointment,  dayBefore);
-        smsDao.save(smsDayBefore);
-        smsReminders.add(smsDayBefore);
+        // Check if day before isn't before now, if so then send sms 2hrs before
+        if (appointment.getStartDate().minusHours(24).isBefore(LocalDateTime.now())) {
+            String twoHoursBefore = getStringDateTwoHoursBefore(appointment);
+            Sms smsTwoHoursBefore = buildSms(appointment, twoHoursBefore);
+            smsDao.save(smsTwoHoursBefore);
+            smsReminders.add(smsTwoHoursBefore);
+        } else {
+            String dayBefore = getStringDateDayBefore(appointment);
+            Sms smsDayBefore = buildSms(appointment,  dayBefore);
+            smsDao.save(smsDayBefore);
+            smsReminders.add(smsDayBefore);
 
-        String twoHoursBefore = getDateTwoHoursBefore(appointment);
-        Sms smsTwoHoursBefore = buildSms(appointment, twoHoursBefore);
-        smsDao.save(smsTwoHoursBefore);
-        smsReminders.add(smsTwoHoursBefore);
+            String twoHoursBefore = getStringDateTwoHoursBefore(appointment);
+            Sms smsTwoHoursBefore = buildSms(appointment, twoHoursBefore);
+            smsDao.save(smsTwoHoursBefore);
+            smsReminders.add(smsTwoHoursBefore);
 
-        appointment.setSmsReminders(smsReminders);
+            appointment.setSmsReminders(smsReminders);
+        }
     }
 
     public void updateSmsReminder(Appointment appointment) throws Exception {
-        SerwerSMS SerwerSMSApi = new SerwerSMS("webapi_merios", "4069ce21");
+        SerwerSMS SerwerSMSApi = new SerwerSMS(apiLogin, apiPassword);
         List<Sms> smses = smsDao.findByAppointmentId(appointment.getId());
         for (Sms sms : smses) {
             SerwerSMSApi.message.delete(sms.getProvidedId(), null);
@@ -46,6 +57,7 @@ public class SmsService {
     }
 
     private Sms buildSms(Appointment appointment, String date) throws Exception {
+        System.out.println(date);
         String providedId = sendSms(appointment, appointment.getClient(), date);
 
         date = date.replace("-", "/");
@@ -61,33 +73,35 @@ public class SmsService {
     }
 
     private String sendSms(Appointment appointment, Client client, String sendDate) throws Exception {
-        SerwerSMS SerwerSMSApi = new SerwerSMS("webapi_merios", "4069ce21");
+        SerwerSMS SerwerSMSApi = new SerwerSMS(apiLogin, apiPassword);
         String type = "json";
         SerwerSMSApi.setFormat(type);
 
         HashMap<String, String> options = new HashMap<>();
-        options.put("test", "true");
+        options.put("test", "false");
         options.put("details", "true");
         options.put("date", sendDate);
 
         String formattedDate = formatDate(appointment.getStartDate());
 
-        String text = "Studio POMALOWANE przypomina o wizycie umówionej na dzień " + formattedDate + ". \nPomalowane";
+        String text = "Gabinet NO I PIEKNIE przypomina o wizycie umówionej na dzień " + formattedDate + ". \n" +
+                "Serdecznie zapraszamy. \n Jesli nie mozesz nas odwiedzic poinformuj o tym minimum 48h przed wizytą.";
         String result = SerwerSMSApi.message.sendSms(client.getPhoneNumber(), text, "Pomalowane", options);
+        System.out.println(result);
 
         return extractSmsId(result);
     }
 
-    private String getDateDayBefore(Appointment appointment) {
+    private String getStringDateDayBefore(Appointment appointment) {
         return appointment.getStartDate().minusHours(24).toString();
     }
 
-    private String getDateTwoHoursBefore(Appointment appointment) {
+    private String getStringDateTwoHoursBefore(Appointment appointment) {
         return appointment.getStartDate().minusHours(2).toString();
     }
 
     private String extractSmsId(String result) {
-        System.out.println(result);
+        //System.out.println(result);
         JSONObject json = new JSONObject(result);
         JSONArray jsonArray = new JSONArray(json.get("items").toString());
         JSONObject tmp = new JSONObject(jsonArray.get(0).toString());
