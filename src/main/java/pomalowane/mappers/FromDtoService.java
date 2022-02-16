5e825600d9ea3774433dad902e621942990e4897
Rafal Szczotka <rafalszczotka@wp.pl>
2022-02-16 00:14:53 +0000

    Added mapper fromDto for Work

--- src/main/java/pomalowane/mappers/FromDtoService.java
+++ src/main/java/pomalowane/mappers/FromDtoService.java
@@ -11,6 +11,7 @@ import pomalowane.work.Work;
 import pomalowane.work.WorkDao;
 import pomalowane.user.User;
 import pomalowane.user.UserDto;
+import pomalowane.work.WorkDto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +28,20 @@ public class FromDtoService {
                 .surname(clientDto.getSurname())
                 .phoneNumber(clientDto.getPhoneNumber())
                 .belatedCounter(clientDto.getBelatedCounter())
+                .isVisible(true)
+                .build();
+    }
+
+    public Work workFromDto(WorkDto workDto) {
+        return Work.builder()
+                .id(workDto.getId())
+                .name(workDto.getName())
+                .price(workDto.getPrice())
+                .providedPrice(workDto.getProvidedPrice())
+                .hoursDuration(workDto.getHoursDuration())
+                .minutesDuration(workDto.getMinutesDuration())
+                .iconName(workDto.getIconName())
+                .providedPrice(workDto.getProvidedPrice())
                 .isVisible(true)
                 .build();
     }
