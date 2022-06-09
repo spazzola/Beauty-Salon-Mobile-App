8b99cd7c41df4b718672b8249fdc000aa252a200
Rafal Szczotka <rafalszczotka@wp.pl>
2022-06-09 16:36:13 +0200

    Added Vacation's validation

--- src/main/java/pomalowane/vacation/VacationService.java
+++ src/main/java/pomalowane/vacation/VacationService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import pomalowane.user.User;
 import pomalowane.user.UserDao;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +23,8 @@ public class VacationService {
 
     @Transactional
     public Vacation createVacation(CreateVacationRequest createVacationRequest) {
+        validateVacation(createVacationRequest.getStartDate(), createVacationRequest.getFinishDate(), createVacationRequest.getEmployeeId());
+
         User employee = userDao.findById(createVacationRequest.getEmployeeId()).get();
 
         Vacation vacation = Vacation.builder()
@@ -35,6 +38,8 @@ public class VacationService {
 
     @Transactional
     public Vacation updateVacation(UpdateVacationRequest updateVacationRequest) {
+        validateVacation(updateVacationRequest.getStartDate(), updateVacationRequest.getFinishDate(), updateVacationRequest.getEmployeeId());
+
         Vacation vacation = vacationDao.getById(updateVacationRequest.getId());
         User employee = userDao.findById(updateVacationRequest.getEmployeeId()).get();
 
@@ -60,6 +65,16 @@ public class VacationService {
     @Transactional
     public void deleteVacation(Long id) {
         vacationDao.deleteById(id);
+    }
+
+    private void validateVacation(LocalDateTime startDate, LocalDateTime finishDate, Long employeeId) {
+        if (finishDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("Bad value of Vacation's finishDate (finishDate can't be before a startDate): " +
+                    "startDate: " + startDate + "\nfinishDate: " + finishDate);
+        }
+        if (employeeId <= 0) {
+            throw new IllegalArgumentException("Bad value of Employee's id: " + employeeId);
+        }
     }
 
 }
